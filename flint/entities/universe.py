biqqles/flint

commit 02c9f975171f68d6355ce4e023c6978a69479d2a

DLL: add "lookup_as_plain" function to convert RDL to plaintext

flint/entities/universe.py
@@ -78,8 +78,9 @@ class Base(Entity):
     system: str
     _market: Dict
 
-    def infocard(self, plain=False) -> str:
-        return self.solar().infocard(plain)
+    def infocard(self, markup='html') -> str:
+        """The infocard of this base's solar (Base sections do not define ids_info)."""
+        return self.solar().infocard(markup)
 
     def system_(self) -> System:
         """The entity of the system this base resides in."""

flint/entities/__init__.py
@@ -19,22 +19,23 @@ from ..formats import dll
 
 @dataclass(kwargs=True, frozen=True)
 class Entity:
-    """The base data class for any entity defined within Freelancer, distinguished by its nickname.
+    """
+    The base data class for any entity defined within Freelancer, distinguished by a nickname.
 
-    Attributes (fields) of subclasses should only be used for the "primitive" attributes that define an entity in the
-    inis. These should reproduce the names exactly as they appear in the inis to enable Entities to be automatically
-    initialised from said files. Fields with clear default values (e.g. navmapscale in universe.ini, which defaults to
-    1.0), dataclasses permitting (only fields at the end of the can have default values). Otherwise, all fields must be
-    provided.
+    Attributes (fields) of Entity classes represent entries defined in the INI file section type which this class
+    represents. This allows Entities to be automatically constructed from parsed INI files. Fields may have default
+    values (defaults are inferred from the input Freelancer expects rather than being defined in the INI files).
 
-    Any attributes not present in the game files should have names beginning with _ to denote their.
-    If you need to extend these classes to include non-standard fields (e.g. mods), you can take the output of ini.parse
-    use dataclasses.from_dict to dynamically define a dataclass and inherit from these classes to get their methods.
+    Methods represent derived fields. For example, the ids_name attribute stores the resource ID of an Entity's name as
+    defined in the INI. The name() method looks up this resource ID in the resource table and returns the string it
+    refers to.
 
-    Derived attributes are represented as methods (ideally these would be properties, but this allows them to be
-    distinguished from primitive attributes).
+    Attributes not present in the INIs but useful to be passed at construction time /can/ be defined, though this should
+    be kept to a minimum. These attributes must have names beginning with _ to denote their internal status.
 
-    All fields must be set, or have a default value, in which case that value will be used."""
+    If you want to extend these classes to cover non-standard fields (e.g. for an unsupported mod), you can use
+    dataclassy.create_dataclass to dynamically define a dataclass and then use these classes as mixins.
+    """
     nickname: str  # a unique string identifier for this entity
     ids_name: int  # resource id for name. note this is occasionally referred to as strid_name in the game files
     ids_info: int  # resource id for infocard
@@ -43,9 +44,16 @@ class Entity:
         """The display name of this entity."""
         return dll.lookup(self.ids_name)
 
-    def infocard(self, plain=False) -> str:
-        """The infocard for this entity, formatted in HTML unless `plain` is specified."""
-        lookup = dll.lookup if plain else dll.lookup_as_html
+    def infocard(self, markup='html') -> str:
+        """The infocard for this entity, formatted in the markup language (`rdl`, `html` or `plain`) specified."""
+        if markup == 'html':
+            lookup = dll.lookup_as_html
+        elif markup == 'plain':
+            lookup = dll.lookup_as_plain
+        elif markup == 'rdl':
+            lookup = dll.lookup
+        else:
+            raise ValueError
         return lookup(self.ids_info)
 
     def __hash__(self) -> int:

flint/entities/goods.py
@@ -16,15 +16,15 @@ from ..formats import dll, utf
 
 
 class Good(Entity):
-    """A Good is anything that can be bought or sold. Commodities, equipment and ships are all examples of goods.
-    (ABSTRACT.)"""
+    """A Good is an abstract representation of something that can be bought or sold. Commodities, equipment and ships
+    all have goods."""
     item_icon: Optional[str]  # path to icon, relative to DATA
     price: int  # the default price for this good, pre market multiplier
     _market: Dict[bool, Tuple]
 
     def icon_path(self) -> str:
         """The absolute path to the .3db file containing this item's icon."""
-        return paths.construct_path('DATA', self.item_icon or 'EQUIPMENT/MODELS/COMMODITIES/NN_ICONS/blank.3db')
+        return paths.construct_path('DATA', self.item_icon or self.DEFAULT_ICON)
 
     def icon(self) -> bytes:
         """This good's icon in TGA format."""
@@ -37,6 +37,8 @@ class Good(Entity):
     def bought_at(self) -> Dict[str, int]:
         """A dict of bases that buy this good of the form {base_nickname: price}."""
         return dict(self._market[False])
+
+    DEFAULT_ICON = 'EQUIPMENT/MODELS/COMMODITIES/NN_ICONS/blank.3db'
 
 
 class Commodity(Good):
@@ -59,9 +61,16 @@ class Ship(Good):
     _hull: Dict[str, Any]
     _package: Dict[str, Any]
 
-    def infocard(self, plain=False) -> str:
+    def infocard(self, markup='html') -> str:
         """I have no idea why the order these are displayed in is not ascending, but anyway."""
-        lookup = dll.lookup if plain else dll.lookup
+        if markup == 'html':
+            lookup = dll.lookup_as_html
+        elif markup == 'plain':
+            lookup = dll.lookup_as_plain
+        elif markup == 'rdl':
+            lookup = dll.lookup
+        else:
+            raise ValueError
         return '<p>'.join(map(lookup, (self.ids_info1, self.ids_info, self.ids_info2, self.ids_info3)))
 
     def type(self) -> str:

flint/entities/solars.py
@@ -8,7 +8,8 @@ file, You can obtain one at http://mozilla.org/MPL/2.0/.
 from typing import Tuple, Optional, Union
 
 from . import Entity
-from .. import formats, maps
+from .. import maps
+from ..formats import dll
 
 
 class Solar(Entity):
@@ -73,10 +74,17 @@ class BaseSolar(Object):
         """The Faction entity that operates this base."""
         return routines.get_factions()[self.reputation]
 
-    def infocard(self, plain=False) -> str:
+    def infocard(self, markup='html') -> str:
         """Base infocards are actually in two parts, with ids_info referring to the specs of a base and ids_info + 1
         storing the actual description"""
-        lookup = formats.dll.lookup if plain else formats.dll.lookup_as_html
+        if markup == 'html':
+            lookup = dll.lookup_as_html
+        elif markup == 'plain':
+            lookup = dll.lookup_as_plain
+        elif markup == 'rdl':
+            lookup = dll.lookup
+        else:
+            raise ValueError
 
         specifications = lookup(self.ids_info)
         try:

flint/formats/dll.py
@@ -10,8 +10,9 @@ well as the logic that Freelancer uses to map those resources to
 internal IDs. Additionally it implements conversion from RDL (used
 for rich-text strings) to HTML.
 """
-from os import SEEK_CUR
 from typing import Dict
+from os import SEEK_CUR
+import xml.etree.ElementTree as xml
 
 import deconstruct as c
 
@@ -25,11 +26,7 @@ resource_table: Dict[int, Dict[int, str]] = {}
 
 @cached
 def lookup(resource_id: int):
-    """
-    Looks up the text associated with a strid in the resource dlls.
-    When a dll is first parsed, its path is replaced in self.dllDict by a dictionary of the form {strid: text}.
-    See parseDLL's docstring for more details.
-    """
+    """Looks up the text associated with a resource ID (string or HTML) in the resource dlls."""
     if resource_id is None:  # sometimes objects which should have infocards don't. Freelancer doesn't seem to care
         return ''
 
@@ -53,11 +50,19 @@ def lookup(resource_id: int):
 
 @cached
 def lookup_as_html(resource_id: int):
-    """Looks up the given resource_id, translates RDL to HTML and returns."""
+    """Looks up the given resource ID and translates RDL to HTML."""
     result = lookup(resource_id)
     for rdl, html in RDL_TO_HTML.items():
         result = result.replace(rdl, html)
     return result
+
+
+@cached
+def lookup_as_plain(resource_id: int):
+    """Looks up the given resource ID and strips out all RDL tags. Paragraph tags are replaced with newlines."""
+    rdl = lookup(resource_id).replace('<PARA/>', '\n').replace('</PARA>', '')
+    tree = xml.fromstring(rdl)
+    return xml.tostring(tree, encoding='unicode', method='text')
 
 
 def parse(path: str, external_strid_offset: int) -> Dict[int, str]:

commit 2e0c0e3ad5061d92d6411f6975ae3bc0065ae9a3

System: fix "region", broken by 02c9f97

flint/entities/universe.py
@@ -68,7 +68,7 @@ class System(Entity):
 
     def region(self) -> str:
         """The name of the region this system is in, extracted from the infocard."""
-        *_, rest = self.infocard(plain=True).partition('<TRA data="1" mask="1" def="-2"/><TEXT>')
+        *_, rest = self.infocard('rdl').partition('<TRA data="1" mask="1" def="-2"/><TEXT>')
         region, *_ = rest.partition('</TEXT>')
         return region.title() if region else 'Unknown'
 