biqqles/flint

commit 2ac16c69d39e3b1e1bbcdd38447f084c397a121a

Dynamic: remove dead code and move remainder to __init__.py

flint/__init__.py
@@ -5,11 +5,32 @@ This Source Code Form is subject to the terms of the Mozilla Public
 License, v. 2.0. If a copy of the MPL was not distributed with this
 file, You can obtain one at http://mozilla.org/MPL/2.0/.
 """
+from types import FunctionType
+from typing import Dict, Any
+from functools import wraps
 import warnings
+
+warnings.formatwarning = lambda message, *args, **kwargs: f'{message!s}\n'  # patch formatter to only show message
+central_cache: Dict[FunctionType, Dict[tuple, Any]] = {}
+
+
+def cached(function):
+    """A decorator which caches a function to the central cache."""
+    central_cache[function] = {}  # initialise cache
+
+    @wraps(function)
+    def wrapped(*args):
+        function_cache = central_cache[function]
+        if args in function_cache:
+            return central_cache[function]
+        result = function_cache[args] = function(*args)
+        return result
+    return wrapped
+
+
 from . import paths
 from .routines import get_commodities, get_bases, get_ships, get_systems, get_factions
 
-warnings.formatwarning = lambda message, *args, **kwargs: f'{message!s}\n'  # patch formatter to only show message
 
 shorthand = {'bases': get_bases,
              'commodities': get_commodities,
@@ -24,4 +45,4 @@ def __getattr__(name):
         raise AssertionError('No path set')
     if name in shorthand:
         return shorthand[name]()
-    raise AttributeError
+    raise AttributeError(f"module {__name__!r} has no attribute {name!r}")

flint/dynamic.py
@@ -1,68 +1,0 @@
-"""
-Copyright (C) 2016, 2017, 2020 biqqles.
-
-This Source Code Form is subject to the terms of the Mozilla Public
-License, v. 2.0. If a copy of the MPL was not distributed with this
-file, You can obtain one at http://mozilla.org/MPL/2.0/.
-
-This file implements the features of dynamic programming used by
-flint.
-"""
-import datetime
-import os
-import pickle
-from functools import wraps
-from types import FunctionType
-from typing import Any, Dict
-
-central_cache: Dict[FunctionType, Dict[tuple, Any]] = {}
-cache_path = 'flint.dat'
-
-
-def write_cache():
-    #central_cache.saved = datetime.datetime.utcnow()
-    with open(cache_path, 'wb') as f:
-        pickle.dump(central_cache, f, protocol=4)
-
-
-def load_cache(expiry_period_days=7):
-    if os.path.isfile(cache_path):
-        with open(cache_path, 'rb') as f:
-            # noinspection PyBroadException
-            # try to load the pickle from file. If it can't be loaded, for any reason, force a reload
-            try:
-                global central_cache
-                if datetime.datetime.utcnow() < central_cache.saved + datetime.timedelta(days=expiry_period_days):
-                    central_cache = pickle.load(f)
-            except Exception:
-                return
-
-
-def cached(function):
-    """A decorator which caches a function to the central cache."""
-    name = function.__module__ + function.__name__
-    central_cache[name] = {}  # initialise cache  # todo: should hash function but pickle is having issues
-
-    @wraps(function)
-    def wrapped(*args):
-        function_cache = central_cache[function.__module__ + function.__name__]
-        if args in function_cache:
-            return central_cache[function.__module__ + function.__name__][args]
-        result = function_cache[args] = function(*args)
-        return result
-    return wrapped
-
-
-# noinspection PyPep8Naming
-class cached_property:
-    """A decorator that replaces the method it is applied to with a property which, when queried, replaces itself
-    with its result, thus "caching" it."""
-    ___slots___ = ('method',)
-
-    def __init__(self, method):
-        self.method = method
-
-    def __get__(self, instance, owner):
-        result = self.method(instance)
-        setattr(instance, self.method.__name__, result)
-        return result

flint/formats/dll.py
@@ -16,7 +16,7 @@ import xml.etree.ElementTree as xml
 
 import deconstruct as c
 
-from ..dynamic import cached
+from .. import cached
 from .. import paths
 from . import WinStruct
 

flint/maps.py
@@ -12,8 +12,7 @@ from collections import namedtuple
 import math
 
 from . import routines
-from .dynamic import cached
-
+from . import cached
 
 PosVector = namedtuple('pos', 'x y z')
 RotVector = namedtuple('rot', 'x y z')

flint/paths.py
@@ -11,7 +11,7 @@ import os
 from typing import Dict, Tuple
 
 from .formats import ini
-from .dynamic import cached
+from . import cached
 
 install: str
 inis: Dict[str, Tuple[str]] = {}  # ini category (defined in freelancer.ini) to a list of paths

flint/routines.py
@@ -12,7 +12,7 @@ from typing import Dict, List, Tuple
 from collections import defaultdict
 
 from . import paths
-from .dynamic import cached
+from . import cached
 from .formats import ini
 from .entities import EntitySet
 from .entities import Commodity, Ship

commit dc582f434230739f6bfae2aae18c35b4122c13da

Fix "cached" decorator, broken in 2ac16c6

flint/__init__.py
@@ -22,7 +22,7 @@ def cached(function):
     def wrapped(*args):
         function_cache = central_cache[function]
         if args in function_cache:
-            return central_cache[function]
+            return function_cache[args]
         result = function_cache[args] = function(*args)
         return result
     return wrapped