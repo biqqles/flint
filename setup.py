4f6000f5e888a84e483d53cb267ac0af3d7f3e39
biqqles <biqqles@protonmail.com>
2021-02-07 21:01:42 +0000

    Update setup.py for v0.8

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='fl-flint',  # distribution name
-    version='0.7.1',
+    version='0.8',
 
     author='biqqles',
     author_email='biqqles@protonmail.com',
