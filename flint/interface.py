f85f5519624272e3d572aec6771e9a721aa2c002
biqqles <biqqles@pm.me>
2022-11-28 20:33:21 +0000

    Fix imports

--- flint/interface.py
+++ flint/interface.py
@@ -7,7 +7,11 @@ file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 Interface-related functions, such as routines for translating RDL.
 """
+from typing import Dict
 import xml.etree.ElementTree as xml
+
+from .formats import ini
+from . import cached, paths
 
 
 def rdl_to_html(rdl: str) -> str:
@@ -35,7 +39,7 @@ def html_to_rdl(html: str) -> str:
 
 @cached
 def get_infocard_map() -> Dict[int, int]:
-    """Return a dict of each ID in infocardmap.ini mapped to the other ID"""
+    """Return a dict of each ID in infocardmap.ini mapped to the other ID."""
     return dict(ini.parse(paths.construct_path('DATA/INTERFACE/infocardmap.ini'))[0][1]['map'])
 
 # A lookup table mapping RDL (Render Display List) tags to HTML(4). Freelancer, to my eternal horror, uses these for
